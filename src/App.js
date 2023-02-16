67cc40f7a8b7ba5e70b77ec97159c5647742e9cc
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-02-16 15:59:33 +0530

    simple useReducer example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { useReducer } from "react";
+
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement'
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.INCREMENT:
+      return { count: state.count + 1 }
+    case ACTIONS.DECREMENT:
+      return { count: state.count - 1 }
+    default:
+      return state
+  }
+}
 
 function App() {
+const [state, dispatch] = useReducer(reducer, { count: 0 })
+
+function increment() {
+  dispatch({ type: ACTIONS.INCREMENT })
+}
+
+function decrement() {
+  dispatch({ type: ACTIONS.DECREMENT })
+}
+
   return (
     <div>
-      <h2 style={{ textAlign: 'center'}}>Hooks</h2>
+      <h2 style={{ textAlign: 'center'}}>useReducer Hook</h2>
+      <button onClick={decrement}>-</button>
+      <span>{state.count}</span>
+      <button onClick={increment}>+</button>
     </div>
   );
 }
